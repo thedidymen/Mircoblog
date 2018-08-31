d79e4569690fdedc05504782ef53182eb68ef646
Reijer van der Zande <rz@warpnet.nl>
2018-08-31 16:47:37 +0200

    chp 8 Followers

--- app/models.py
+++ app/models.py
@@ -5,6 +5,12 @@ from flask_login import UserMixin
 from app import login
 from hashlib import md5
 
+
+followers = db.Table('folowers',
+	db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+	db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+	)
+
 class User(UserMixin, db.Model):
 	"""docstring for User"""
 	id = db.Column(db.Integer, primary_key=True)
@@ -14,6 +20,10 @@ class User(UserMixin, db.Model):
 	posts = db.relationship('Post', backref='author', lazy='dynamic')
 	about_me = db.Column(db.String(140))
 	last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+	followed = db.relationship('User', secondary=followers, 
+		primaryjoin=(followers.c.follower_id == id), 
+		secondaryjoin=(followers.c.followed_id == id), 
+		backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
 	def __repr__(self):
 		return '<User {}>'.format(self.username)
@@ -26,9 +36,26 @@ class User(UserMixin, db.Model):
 
 	def avatar(self, size):
 
-		# digest = md5(self.email.lower().encode('utf-8')).hexdigest()
-		digest = md5('thedidymen@hotmail.com'.encode('utf-8')).hexdigest()
+		digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+		# digest = md5('thedidymen@hotmail.com'.encode('utf-8')).hexdigest()
 		return 'https://www.gravatar.com/avatar/{}?d=indenticon&s={}'.format(digest, size)
+
+	def follow(self, user):
+		if not self.is_following(user):
+			self.followed.append(user)
+
+	def unfollow(self, user):
+		if self.is_following(user):
+			self.followed.remove(user)
+
+	def is_following(self, user):
+		return self.followed.filter(followers.c.followed_id == user.id).count() > 0
+
+	def followed_posts(self):
+		followed = Post.query.join(followers, (followers.c.followed_id == Post.user_id)).filter(followers.c.follower_id == self.id)
+		own = Post.query.filter_by(user_id=self.id)
+		return followed.union(own).order_by(Post.timestamp.desc())
+
 
 class Post(db.Model):
 	"""docstring for Post"""
@@ -39,6 +66,7 @@ class Post(db.Model):
 
 	def __repr__(self):
 		return '<Post {}>'.format(self.body)
+
 		
 @login.user_loader
 def load_user(id):
--- app/routes.py
+++ app/routes.py
@@ -1,26 +1,24 @@
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from app import app, db
-from app.models import User
-from app.forms import RegistrationForm, EditProfileForm, LoginForm
+from app.models import User, Post
+from app.forms import RegistrationForm, EditProfileForm, LoginForm, PostForm
 from werkzeug.urls import url_parse
 from datetime import datetime
 
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
-	posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
+	form = PostForm()
+	if form.validate_on_submit():
+		post = Post(body=form.post.data, author=current_user)
+		db.session.add(post)
+		db.session.commit()
+		flash('Your post is now live!')
+		return redirect(url_for('index'))
+	posts = current_user.followed_posts().all()
 	return render_template('index.html', title='Epic Adventure', posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -78,7 +76,7 @@ def before_request():
 @app.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
 def edit_profile():
-	form = EditProfileForm()
+	form = EditProfileForm(current_user.username)
 	if form.validate_on_submit():
 		current_user.username = form.username.data
 		current_user.about_me = form.about_me.data
@@ -89,3 +87,33 @@ def edit_profile():
 		form.username.data	= current_user.username
 		form.about_me.data = current_user.about_me
 	return render_template('edit_profile.html', title='Edit Profile', form=form)
+
+@app.route('/follow/<username>')
+@login_required
+def follow(username):
+	user = User.query.filter_by(username=username).first()
+	if user is None:
+		flash('User {} not found.'.format(username))
+		return redirect(url_for('index'))
+	if user == current_user:
+		flash('You cannot follow yourself!')
+		return redirect(url_for('user', username=username))
+	current_user.follow(user)
+	db.session.commit()
+	flash('You are following {}!'.format(username))
+	return redirect(url_for('user', username=username))
+
+@app.route('/unfollow/<username>')
+@login_required
+def unfollow(username):
+	user = User.query.filter_by(username=username).first()
+	if user is None:
+		flash('User {} not found.'.format(username))
+		return redirect(url_for('index'))
+	if user == current_user:
+		flash('You cannot unfollow yourself!')
+		return redirect(url_for('user', username=username))
+	current_user.unfollow(user)
+	db.session.commit()
+	flash('You are not following {}.'.format(username))
+	return redirect(url_for('user', username=username))
